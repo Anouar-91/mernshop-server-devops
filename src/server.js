73f76935ea0e34c61691ad42ea8694ed77090c62
Anouar-91 <a.berrouane@hotmail.com>
2023-02-22 20:49:37 +0100

    add url front

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ app.use(express.json())
 
 let cors = require("cors");
 app.use(cors({
-    origin: [ "https://brilliant-licorice-c691cc.netlify.app","https://mernshop.herokuapp.com",process.env.CLIENT_URL, "http://localhost:3000", "*"],
+    origin: [ "http://192.168.1.24:3000","https://brilliant-licorice-c691cc.netlify.app","https://mernshop.herokuapp.com",process.env.CLIENT_URL, "http://localhost:3000", "*"],
     methods: ["GET", "POST", "HEAD", "PUT", "PATCH", "DELETE"],
     credentials: true,
 }));
